9e8ba63a275db297149b573c64877d570efcc062
cid-509 <kagenoc507@gmail.com>
2025-02-14 01:39:16 -0500

    Update config.js

--- config.js
+++ config.js
@@ -19,7 +19,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "PUT YOUR SESSION ID HERE",
+SESSION_ID: process.env.SESSION_ID || "ecBl3YRY#o-AM7-AyQIXdyr4ZKgMqRuccRHajTu7IcOJ1xs8SYhg",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 MODE: process.env.MODE || "private",
 AUTO_VOICE: process.env.AUTO_VOICE || "false",
